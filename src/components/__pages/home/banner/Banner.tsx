7a41743a59de4ecd3d7dccfd1dd1c71181673082
Cuong Hoang <csshandler.pt@gmail.com>
2022-09-02 02:25:17 +0700

    home banner background

--- src/components/__pages/home/banner/Banner.tsx
+++ src/components/__pages/home/banner/Banner.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Banner: React.FC = () => {
   return (
-    <div className="h-banner mobile-645px:h-200px bg-cyan-400 flex flex-col justify-center items-center mobile-950px:pl-16px mobile-950px:pr-16px">
+    <div
+      style={{ backgroundImage: "url(/images/banner-home.png)" }}
+      className="bg-center bg-no-repeat bg-cover  h-banner mobile-645px:h-200px bg-cyan-400 flex flex-col justify-center items-center mobile-950px:pl-16px mobile-950px:pr-16px"
+    >
       <div className="mobile-950px:text-25px mobile-950px:mb-16px font-bold font-pop text-cs-secondary-200 text-48px leading-72 text-center mb-52px">
         Movie information hub
       </div>
