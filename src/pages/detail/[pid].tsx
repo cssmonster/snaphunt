d64f956401fc3195967b5a52569be7fd6e1dbca8
Cuong Hoang Van <cuong.hoang@idealogic.com.vn>
2022-09-01 21:52:24 +0700

    detail page

--- src/pages/detail/[pid].tsx
+++ src/pages/detail/[pid].tsx
@@ -18,7 +18,208 @@ export default function List() {
             Deadpool
           </div>
 
-          <div className="flex justify-start items-center"></div>
+          <div className="flex justify-start items-center mb-24px flex-wrap">
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+            <button className="mr-16px mb-16px pt-10px pb-10px pl-4px pr-4px border-2 border-cs-secondary-400 font-pop text-14px leading-14px">
+              Category
+            </button>
+          </div>
+
+          <p className="mb-16px font-pop text-14px leading-21px">
+            Simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry"s standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged. It was popularised in the 1960s with the
+            release of Letraset sheets containing Lorem Ipsum passages, and more
+            recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.Simply dummy text of the printing
+            and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.Simply dummy text of the printing and typesetting industry.
+            <br />
+            <br />
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.Simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+
+          <div className="text-black font-pop text-36px leading-54px font-bold mb-16px">
+            Cast
+          </div>
+
+          <div className="border-2 border-black pt-50px pb-50px pl-38px pr-38px flex flex-wrap justify-between items-start">
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex  justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+            <div className="mobile-700px:w-full res-1103px:w-half w-tri flex justify-start items-center mb-32px">
+              <div
+                style={{
+                  backgroundImage: `url(https://tieudung.kinhtedothi.vn/upload_images/images/2018/05/19/Deadpool-2.jpg)`,
+                }}
+                className="mr-16px w-93px h-93px rounded-max  bg-center bg-no-repeat bg-cover"
+              ></div>
+              <div className="font-pop text-24px leading-36px">
+                Original Name <br />
+                Character Name
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </DefaultLayout>
